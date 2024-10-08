1faf32543a78b02fbf05f65c871c4170c0fc7957
likithanalajala <162860256+likithanalajala@users.noreply.github.com>
2024-10-08 21:48:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// // JavaScript for button clicks and redirects
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Admin Module Redirect
+//     document.getElementById('adminBtn').addEventListener('click', function() {
+//         window.location.href = 'admin.html';
+//     });
+
+//     // Citizen Module Redirect
+//     document.getElementById('citizenBtn').addEventListener('click', function() {
+//         window.location.href = 'citizen.html';
+//     });
+
+//     // Politician Module Redirect
+//     document.getElementById('politicianBtn').addEventListener('click', function() {
+//         window.location.href = 'politician.html';
+//     });
+
+//     // Moderator Module Redirect
+//     document.getElementById('moderatorBtn').addEventListener('click', function() {
+//         window.location.href = 'moderator.html';
+//     });
+// });
